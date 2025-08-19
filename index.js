aa10b8a57289ba8cdcf81a19623a23fc2475cbd4
Ayodeji Adebolu <ayodejiadebolu@gmail.com>
2025-08-19 11:07:36 +0100

    allow cors for localhost

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(
   cors({
-    origin: [
-      'https://degeniusfxacademy.netlify.app',
-      // 'http://localhost:5173'
-    ],
+    origin: ['https://degeniusfxacademy.netlify.app', 'http://localhost:5173'],
     credentials: true,
   })
 );
